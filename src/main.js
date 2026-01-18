9fbfd496d9cad645789b5d51aba9c63a44e828c1
2EZ4BLINK <blinkcode24@gmail.com>
2026-01-18 18:04:33 +0800

    added more details like pane metalness shininess more materials

--- src/main.js
+++ src/main.js
@@ -1,40 +1,63 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { Pane } from "tweakpane";
+
+// PANE
+const pane = new Pane();
 
 // Create a scene
 const scene = new THREE.Scene();
 
-//add objects to he scene
+// GEOMETRY
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({
-  color: "red",
-  wireframe: true,
-});
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+const torusKnot = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
+
+// MATERIAL
+const material = new THREE.MeshPhongMaterial();
+material.shininess = 9999;
+material.color = new THREE.Color("red");
+const standardMaterial = new THREE.MeshStandardMaterial();
+standardMaterial.color = new THREE.Color("blue");
+
+// MESH
+const cubeMesh = new THREE.Mesh(torusKnot, standardMaterial);
+cubeMesh.position.x = 1;
+const mesh2 = new THREE.Mesh(cubeGeometry, material);
+mesh2.position.x = -1;
+const mesh3 = new THREE.Mesh(cubeGeometry, standardMaterial);
+mesh3.position.y = -1.5;
+
 scene.add(cubeMesh);
-cubeMesh.position.set(0, 1, 0);
-const axesHelper = new THREE.AxesHelper(3);
-cubeMesh.add(axesHelper);
-cubeMesh.rotation.reorder("YXZ");
-cubeMesh.rotation.y = THREE.MathUtils.degToRad(90);
-cubeMesh.rotation.x = THREE.MathUtils.degToRad(45);
+scene.add(mesh2);
+scene.add(mesh3);
+
+// Add Pane
+const PARAMS = {
+  min: 0,
+  max: 1,
+  step: 0.01,
+};
+
+pane.addBinding(standardMaterial, "metalness", PARAMS);
+pane.addBinding(standardMaterial, "roughness", PARAMS);
+
+// LIGHT;
+const light = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(light);
+const pointLight = new THREE.PointLight(0xffffff, 0.9);
+pointLight.position.set(1, 1, 1);
+scene.add(pointLight);
+
+// add objects to the scene
+
 // initialize camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  35,
   window.innerWidth / window.innerHeight,
   0.1,
-  30,
+  200,
 );
 camera.position.z = 5;
-
-// const camera = new THREE.OrthographicCamera(
-//   (-1 * window.innerWidth) / window.innerHeight,
-//   (1 * window.innerWidth) / window.innerHeight,
-//   1,
-//   -1,
-//   0.1,
-//   1000,
-// );
 
 // initialize renderer
 const canvas = document.querySelector(".threejs");
@@ -52,6 +75,7 @@ window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+// render the scene
 const renderLoop = () => {
   controls.update();
   renderer.render(scene, camera);
